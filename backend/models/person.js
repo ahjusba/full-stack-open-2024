bad99bad9f58d34dc7d1be38dd910e16e418f4eb
ahjusba <jussi.rislakki@gmail.com>
2024-08-12 21:08:56 +0300

    Add personScheme validation

--- backend/models/person.js
+++ backend/models/person.js
@@ -7,24 +7,40 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-  })
-  
-  personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(value) {
+        //eg. 050-123123 or 09-0011
+        const regex = /^\d{2,3}-\d{5,}$/
+        return regex.test(value)
+      },
+      message: props => `${props.value} not a valid format. Expected format is "number-number", eg. "050-112233"`
+    },
+    required: true,
+  }
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Person', personSchema)
