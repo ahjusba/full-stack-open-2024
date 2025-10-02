e114c90c54fdb6f5d1ba517808225a930844ac6c
Rislakki <jussi.rislakki@sogeti.com>
2025-10-02 16:47:17 +0300

    Starting exercise 9.10

--- osa9/Patientor/Backend/src/index.ts
+++ osa9/Patientor/Backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisRouter from './routes/diagnoses';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use('/api/diagnoses', diagnosisRouter);
 
 const PORT = 3001;
 
