a53990be3a983860bbf1484e7c3985adbc2caa86
Rislakki <jussi.rislakki@sogeti.com>
2025-10-06 13:35:15 +0300

    Add zod

--- osa9/FlightDiaries/src/utils.ts
+++ osa9/FlightDiaries/src/utils.ts
@@ -1,65 +1,75 @@
 import { NewDiaryEntry, Weather, Visibility } from './types';
+import z from 'zod';
+
+const newEntrySchema = z.object({
+  commnet: z.string().optional(),
+  date: z.iso.date(),
+  weather: z.enum(Weather),
+  visibility: z.enum(Visibility)
+});
 
 const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
 
-  if (!object || typeof object !== 'object') {
-    throw new Error('Incorrect or missing data');
-  }
+  return newEntrySchema.parse(object);
 
-  if ('comment' in object && 'date' in object && 'weather' in object && 'visibility' in object) {
-    const newEntry: NewDiaryEntry = {
-      comment: parseComment(object.comment),
-      date: parseDate(object.date),
-      weather: parseWeather(object.weather),
-      visibility: parseVisibility(object.visibility)
-    };
-    return newEntry;
-  };
-  throw new Error('Incorrect data: some fields are missing');
-};
+  // if (!object || typeof object !== 'object') {
+  //   throw new Error('Incorrect or missing data');
+  // }
 
-const parseVisibility = (visibility: unknown): Visibility => {
-  if (!isString(visibility) || !isVisibility(visibility)) {
-    throw new Error('Incorrect or missing visibility: ' + visibility);
-  }
-  return visibility;
-};
-
-const parseWeather = (weather: unknown): Weather => {
-  if (!isString(weather) || !isWeather(weather)) {
-    throw new Error('Incorrect or missing weather: ' + weather);
-  }
-  return weather;
-};
-
-const parseComment = (comment: unknown): string => {
-  if (!isString(comment)) {
-    throw new Error('Incorrect or missing comment: ' + comment);
-  }
-  return comment;
-};
-
-const parseDate = (date: unknown): string => {
-  if (!isString(date) || !isDate(date)) {
-    throw new Error('Incorrect or missing date: ' + date);
-  }
-  return date;
-};
-
-const isVisibility = (param: string): param is Visibility => {
-  return Object.values(Visibility).map(v => v.toString()).includes(param);
-};
-
-const isWeather = (param: string): param is Weather => {
-  return Object.values(Weather).map(v => v.toString()).includes(param);
-};
-
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
-const isString = (text: unknown): text is string => {
-  return typeof text === 'string' || text instanceof String;
+  // if ('comment' in object && 'date' in object && 'weather' in object && 'visibility' in object) {
+  //   const newEntry: NewDiaryEntry = {
+  //     comment: z.string().parse(object.comment),
+  //     date: z.iso.date().parse(object.date),
+  //     weather: z.enum(Weather).parse(object.weather),
+  //     visibility: z.enum(Visibility).parse(object.visibility)
+  //   };
+  //   return newEntry;
+  // };
+  // throw new Error('Incorrect data: some fields are missing');
 };
 
 export default toNewDiaryEntry;
+
+// const parseVisibility = (visibility: unknown): Visibility => {
+//   if (!isString(visibility) || !isVisibility(visibility)) {
+//     throw new Error('Incorrect or missing visibility: ' + visibility);
+//   }
+//   return visibility;
+// };
+
+// const parseWeather = (weather: unknown): Weather => {
+//   if (!isString(weather) || !isWeather(weather)) {
+//     throw new Error('Incorrect or missing weather: ' + weather);
+//   }
+//   return weather;
+// };
+
+// const parseComment = (comment: unknown): string => {
+//   if (!isString(comment)) {
+//     throw new Error('Incorrect or missing comment: ' + comment);
+//   }
+//   return comment;
+// };
+
+// const parseDate = (date: unknown): string => {
+//   if (!isString(date) || !isDate(date)) {
+//     throw new Error('Incorrect or missing date: ' + date);
+//   }
+//   return date;
+// };
+
+// const isVisibility = (param: string): param is Visibility => {
+//   return Object.values(Visibility).map(v => v.toString()).includes(param);
+// };
+
+// const isWeather = (param: string): param is Weather => {
+//   return Object.values(Weather).map(v => v.toString()).includes(param);
+// };
+
+// const isDate = (date: string): boolean => {
+//   return Boolean(Date.parse(date));
+// };
+
+// const isString = (text: unknown): text is string => {
+//   return typeof text === 'string' || text instanceof String;
+// };
