4f78038138db739f5437af37649905b9c076057a
Rislakki <jussi.rislakki@sogeti.com>
2025-10-07 11:40:07 +0300

    Initialize frontend project

--- osa9/Ts-front/react-ts/src/App.tsx
+++ osa9/Ts-front/react-ts/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface CoursePartBase {
   name: string;
   exerciseCount: number;
@@ -115,49 +117,52 @@ const Total = ({ total }: TotalProps) => (
   <h1>Number of exercises {total}</h1>
 );
 
-const App = () => {
-  const courseName = "Half Stack application development";
-  const courseParts: CoursePart[] = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10,
-      description: "This is an awesome course part",
-      kind: "basic"
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7,
-      groupProjectCount: 3,
-      kind: "group"
-    },
-    {
-      name: "Basics of type Narrowing",
-      exerciseCount: 7,
-      description: "How to go from unknown to string",
-      kind: "basic"
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14,
-      description: "Confusing description",
-      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
-      kind: "background"
-    },
-    {
-      name: "TypeScript in frontend",
-      exerciseCount: 10,
-      description: "a hard part",
-      kind: "basic",
-    },
-    {
-      name: "Backend development",
-      exerciseCount: 21,
-      description: "Typing the backend",
-      requirements: ["nodejs", "jest"],
-      kind: "special"
-    }
-  ];
+const parts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is an awesome course part",
+    kind: "basic"
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    kind: "group"
+  },
+  {
+    name: "Basics of type Narrowing",
+    exerciseCount: 7,
+    description: "How to go from unknown to string",
+    kind: "basic"
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+    kind: "background"
+  },
+  {
+    name: "TypeScript in frontend",
+    exerciseCount: 10,
+    description: "a hard part",
+    kind: "basic",
+  },
+  {
+    name: "Backend development",
+    exerciseCount: 21,
+    description: "Typing the backend",
+    requirements: ["nodejs", "jest"],
+    kind: "special"
+  }
+];
 
+const App = () => {
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [courseParts, setCourseParts] = useState(parts)
+  const courseName = "Half Stack application development";
   const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
 
   return (
