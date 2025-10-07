e3bee94638e6eaff0556b101e9b5c6d4276cd137
Rislakki <jussi.rislakki@sogeti.com>
2025-10-07 10:53:23 +0300

    Add exercise 9.16

--- osa9/Ts-front/react-ts/src/App.tsx
+++ osa9/Ts-front/react-ts/src/App.tsx
@@ -1,7 +1,34 @@
-interface CoursePart {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
+
+interface CoursePartRequirements extends CourseDescription {
+  requirements: string[];
+  kind: "special"
+}
+
+interface CourseDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartBasic extends CourseDescription {
+  description: string;
+  kind: "basic"
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+interface CoursePartBackground extends CourseDescription {
+  description: string;
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartRequirements;
 
 interface HeaderProps {
   courseName: string;
@@ -15,6 +42,61 @@ interface TotalProps {
   total: number;
 }
 
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.kind) {
+    case "basic":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>Kind: {part.kind}</p>
+          <p>Description: {part.description}</p>
+        </div>
+      );
+    case "group":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>Kind: {part.kind}</p>
+          <p>Project Count: {part.groupProjectCount}</p>
+        </div>
+      );
+    case "background":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>Kind: {part.kind}</p>
+          <p>Description: {part.description}</p>
+          <p>Material: {part.backgroundMaterial}</p>
+        </div>
+      );
+    case "special":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>Kind: {part.kind}</p>
+          <p>Description: {part.description}</p>
+          <p>Requirements:</p>
+          {part.requirements.map((req, index) => (
+            <ul key={index}>
+              {req}
+            </ul>
+          ))}
+        </div>
+      );
+    default:
+      return assertNever(part);
+  }
+};
+
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled kind: ${JSON.stringify(value)}`);
+};
+
+
 const Header = ({ courseName }: HeaderProps) => (
   <h1>{courseName}</h1>
 );
@@ -23,14 +105,14 @@ const Content = ({ courseParts }: ContentProps) => (
   <>
     {courseParts.map((part, index) => (
       <p key={index}>
-        {part.name} {part.exerciseCount}
+        <Part part={part} />
       </p>
     ))}
   </>
 );
 
 const Total = ({ total }: TotalProps) => (
-  <p>Number of exercises {total}</p>
+  <h1>Number of exercises {total}</h1>
 );
 
 const App = () => {
@@ -38,15 +120,41 @@ const App = () => {
   const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
 
